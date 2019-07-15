ebfa510c7cfc9e3a698b9dd61fe99f46d26f0ebc
waheng chang <wahengchang@gmail.com>
2019-07-15 23:32:43 +0800

    no message

--- src/google-art-download-python/tile_fetch.py
+++ src/google-art-download-python/tile_fetch.py
@@ -121,9 +121,9 @@ async def load_tiles(url, _z=-1, outfile=None):
     img = Image.new(mode="RGB", size=level.size)
 
     print('-=-=-=-=-=2 ')
-    tiles_dir = Path('./tmp/'+info.image_name)
+    tiles_dir = Path('tmp/'+info.image_name)
     print('-=-=-=-=-=3 ')
-    tiles_dir.mkdir(exist_ok=True)
+    tiles_dir.mkdir(parents=True, exist_ok=True)
 
     print('-=-=-=-=-=4 ')
 
